4470d7fd6840429db7c06c77c38e1194ffabf2db
JCSzamosi <jc.szamosi@alumni.utoronto.ca>
2021-03-15 16:58:01 -0400

    Tabulated for March 16 2021

--- get_old.sh
+++ get_old.sh
@@ -3,6 +3,6 @@
 file='data/current_week.csv'
 while IFS=$'\t' read hash ts
 do
-	print -v fn '%(%Y-%m-%d)T-%s' "$ts" "$(file##*/}"
+	printf -v fn '%(%Y-%m-%d)T-%s' "$ts" "${file##*/}"
 	git cat-file -p "${hash}:${file}" > "$fn"
 done < <(git log --reverse --format=$'%H\t%ct' -- "$file")

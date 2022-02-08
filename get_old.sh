6799d16b23e63fc9ab7f6d0f66145a971f6d37f6
JCSzamosi <jc.szamosi@alumni.utoronto.ca>
2022-02-07 20:08:20 -0500

    start looking at history

--- get_old.sh
+++ get_old.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-file='data/current_week.csv'
+file='data/current_movies.csv'
 while IFS=$'\t' read hash ts
 do
 	printf -v fn '%(%Y-%m-%d)T-%s' "$ts" "${file##*/}"
 	git cat-file -p "${hash}:${file}" > "$fn"
 done < <(git log --reverse --format=$'%H\t%ct' -- "$file")
+
